589023597806acf2bc595a5e535a7c9b7de5003d
gEc4AwAlgroA <gEc4AwAlgroA@ccaygill.com>
2014-08-11 00:24:06 +0100

    plot1

---
+++ plot1.R
@@ -0,0 +1,5 @@
+alldata<-read.csv("household_power_consumption.txt", sep=";")
+data<-alldata[alldata$Date=="1/2/2007" | alldata$Date=="2/2/2007",]
+hist(as.numeric(data$Global_active_power)/1000, col="red", xlab="Global Active Power (kW)", main="Global Active Power")
+dev.copy(png, "plot1.png")
+dev.off()
